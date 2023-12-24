d3b6b3347b96192605cd3db63b26b07ae7720d2a
league <league@imac-28.attlocal.net>
2023-12-23 16:27:43 -0800

    a

--- src/_03_typing_tutor/Typing_Tutor.java
+++ src/_03_typing_tutor/Typing_Tutor.java
@@ -1,15 +1,20 @@
 package _03_typing_tutor;
 
+import java.awt.Color;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Typing_Tutor {
 	public static void main(String[] args){
 		Typing_Tutor tutor = new Typing_Tutor();
-		tutor.setup(); 
+		while(0==0) {
+			tutor.setup(); 
+			tutor.keyPressed();
+		}
 	}
 
 	char generateRandomLetter() {
@@ -19,13 +24,15 @@ public class Typing_Tutor {
 
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	char currentLetter;
-
+	static char currentLetter;
+	JLabel label = new JLabel();
 	public void setup(){
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		currentLetter = generateRandomLetter();
-		JLabel label = new JLabel(currentLetter);
+		JLabel label = new JLabel();
+		label.setText(null);
+		label.setText(String. valueOf(currentLetter)+" ");
 		label.setFont(label.getFont().deriveFont(28.0f));
 		label.setHorizontalAlignment(JLabel.CENTER);
 		label.addKeyListener(null);
@@ -33,8 +40,19 @@ public class Typing_Tutor {
 		frame.add(panel);
 		frame.pack();
 	}
-	public void keyReleased() {
-		char currentLetter = generateRandomLetter();
-		label.setText(currentLetter);
+
+	public void keyPressed() {
+		String pressedKey=JOptionPane.showInputDialog("what is the letter:");
+		System.out.println("you typed:"+pressedKey);
+		char c=pressedKey.charAt(0);
+		if (c==currentLetter) {
+			System.out.println("Correct");
+			panel.setBackground(Color.GREEN);
+		}
+		else{
+			System.out.println("The correct answer was:"+currentLetter);
+			System.out.println("False");
+			panel.setBackground(Color.RED);
+		}
 	}
 }
