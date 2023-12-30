050ae0caf6bc30179664133ca4c561c54c5f9128
jereycui2008 <jeremycuid@gmail.com>
2023-12-30 11:26:24 -0800

    rfwqf

--- src/_04_book_of_illusions/BookOfIllusions.java
+++ src/_04_book_of_illusions/BookOfIllusions.java
@@ -13,11 +13,13 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
+import com.sun.prism.Image;
  
 /** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
 
 public class BookOfIllusions extends MouseAdapter {
-
+	JLabel newJLabel=new JLabel();
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
 	JFrame frame=new JFrame();
 	public void run() {
@@ -28,24 +30,31 @@ public class BookOfIllusions extends MouseAdapter {
 		// 4. find 2 images and save them to your project’s _06_book_of_illusions folder
 		
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
-		
+		String sillyCat1="sillyCat1.jpeg";
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
-		
 		// 7. use the "loadImage..." methods below to initialize your JLabel
-		
+		newJLabel=loadImageFromComputer(sillyCat1);
 		// 8. add your JLabel to the frame
-		
+		frame.add(newJLabel);
 		// 9. call the pack() method on the frame
-		
+		frame.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
-		
+		frame.addMouseListener(this);
 	}
 
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
+		System.out.println("Clicked!");
 		// 12. remove everything from the frame that was added earlier
+		frame.remove(newJLabel);
 		// 13. load a new image like before (this is more than one line of code)
+		frame.setSize(100,100);
+		String sillyCat2="sillyCat2.jpeg";
+		JLabel newJLabel2=new JLabel();
+		newJLabel2=loadImageFromComputer(sillyCat2);
+		frame.add(newJLabel2);
 		// 14. pack the frame
+		frame.pack();
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
