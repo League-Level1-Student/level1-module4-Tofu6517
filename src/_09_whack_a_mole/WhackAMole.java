54666a77acbab96e10abc5334ccc0c13efc26a72
league <maddoxseul@sbsd.net>
2024-01-27 16:28:52 -0800

    a

--- src/_09_whack_a_mole/WhackAMole.java
+++ src/_09_whack_a_mole/WhackAMole.java
@@ -1,69 +1,55 @@
 package _09_whack_a_mole;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class WhackAMole {
+public class WhackAMole implements ActionListener{
 	public static void main(String[] args) {
 		WhackAMole wAM=new WhackAMole();
 		wAM.moleSetUp();
+		wAM.drawButtons();
 	}
+	Random rn = new Random();
+	int answer = rn.nextInt(24) + 1;
 	JPanel newPanel=new JPanel();
-	JButton b1=new JButton();
-	JButton b2=new JButton();
-	JButton b3=new JButton();
-	JButton b4=new JButton();
-	JButton b5=new JButton();
-	JButton b6=new JButton();
-	JButton b7=new JButton();
-	JButton b8=new JButton();
-	JButton b9=new JButton();
-	JButton b10=new JButton();
-	JButton b11=new JButton();
-	JButton b12=new JButton();
-	JButton b13=new JButton();
-	JButton b14=new JButton();
-	JButton b15=new JButton();
-	JButton b16=new JButton();
-	JButton b17=new JButton();
-	JButton b18=new JButton();
-	JButton b19=new JButton();
-	JButton b20=new JButton();
-	JButton b21=new JButton();
-	JButton b22=new JButton();
-	JButton b23=new JButton();
-	JButton b24=new JButton();
+	JButton pressed = createButton(""); 
 	public void moleSetUp() {
-		JFrame frame=new JFrame("Pig Latin");
+		JFrame frame=new JFrame("Wak.A.Mole");
 		frame.setVisible(true);
-		frame.setSize(200,80);
+		frame.setSize(310,330);
 		frame.add(newPanel);
-		newPanel.add(b1);
-		newPanel.add(b2);
-		newPanel.add(b3);
-		newPanel.add(b4);
-		newPanel.add(b5);
-		newPanel.add(b6);
-		newPanel.add(b7);
-		newPanel.add(b8);
-		newPanel.add(b9);
-		newPanel.add(b11);
-		newPanel.add(b12);
-		newPanel.add(b13);
-		newPanel.add(b14);
-		newPanel.add(b15);
-		newPanel.add(b16);
-		newPanel.add(b17);
-		newPanel.add(b18);
-		newPanel.add(b19);
-		newPanel.add(b20);
-		newPanel.add(b21);
-		newPanel.add(b22);
-		newPanel.add(b23);
-		newPanel.add(b24);
+	}
+	public int theRN() {
+		Random rn = new Random();
+		int answer = rn.nextInt(24) + 1;
+		return answer;
+		}
+	public void drawButtons() {
+		
+		for(int i=1;i<24;i++) {
+			JButton normalButton = createButton(""); 
+			if(i==answer) {
+				pressed.setText("MOLE!");
+				newPanel.add(pressed);
+			}
+			newPanel.add(normalButton);
+		}
+	}
+	private JButton createButton(String p) {
+		JButton theButton=new JButton();
+		return theButton;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==pressed) {
+			drawButtons();
+		}
 	}
 }
